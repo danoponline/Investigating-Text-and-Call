a06cfe5b080625e8701d520babac15096c94c4ea
Danop Rajabhandharaks <danoponline@Jasons-MacBook-Pro.local>
2020-05-07 14:14:59 -0700

    Task1 1st completion

--- Task1.py
+++ Task1.py
@@ -18,3 +18,35 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+number_distinct_list =[]
+
+#For loop to go through each line in texts
+for index in range(len(texts)):
+    if len(number_distinct_list) == 0:
+        number_distinct_list.append(texts[index][0])
+    else:
+        #Hidden loop here to go through each element in number_distinct_list
+        #Worst case can be the same length as the number of line in texts
+        if texts[index][0] not in number_distinct_list:
+            number_distinct_list.append(texts[index][0])
+    
+    #Another hidden nested loop here
+    if texts[index][1] not in number_distinct_list:
+        number_distinct_list.append(texts[index][1])
+
+#For loop to go through each line in texts
+for index in range(len(calls)):
+    if len(number_distinct_list) == 0:
+        number_distinct_list.append(calls[index][0])
+    else:
+        #Hidden nested loop here to go through each element in number_distinct_list
+        #Worst case can be the same length as the number of line in texts
+        if calls[index][0] not in number_distinct_list:
+            number_distinct_list.append(calls[index][0])
+    
+    #Another hidden nested loop here
+    if calls[index][1] not in number_distinct_list:
+        number_distinct_list.append(calls[index][1])
+
+count = len(number_distinct_list)
+print("There are " + format(count) + " different telephone numbers in the records.")
