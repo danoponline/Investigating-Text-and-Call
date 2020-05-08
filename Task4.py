134b3aa1dfe2da936713cf81bc9fc3bf47faa8d0
Danop Rajabhandharaks <drajabhandharaks@scu.edu>
2020-05-07 22:52:45 -0700

    1st submission for Task4.py

--- Task4.py
+++ Task4.py
@@ -25,3 +25,61 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+# Create a list of phone number that is not telemarketing by appending all the distinct number
+# from texts list and receiving number in calls list
+# Worst-case time complexity is O(n^2)
+def creatNotTeleMarketList(texts_list,calls_list):
+    not_telemarket_list = []
+    
+    # Loop in texts_list
+    # Worst-case time complexity of this loop is O(n^2)
+    for line in texts_list:
+        if len(not_telemarket_list) == 0:
+            not_telemarket_list.append(line[0])
+            not_telemarket_list.append(line[1])
+        else:
+            if line[0] not in not_telemarket_list:
+                not_telemarket_list.append(line[0])
+            if line[1] not in not_telemarket_list:
+                not_telemarket_list.append(line[1])
+    
+    # Loop in calls_list
+    # Worst-case time complexity of this loop is O(n^2)
+    for line in calls_list:
+        if len(not_telemarket_list) == 0:
+            not_telemarket_list.append(line[1])
+        else:
+            if line[1] not in not_telemarket_list:
+                not_telemarket_list.append(line[1])
+
+    return not_telemarket_list
+
+# Start of main program
+# This is a list that will capture potential telemarketing
+potential_telemarket_list = []
+
+# Call function to create not telemarket list
+# Worst-case time complexity is O(n^2)
+not_telemarket_list = creatNotTeleMarketList(texts,calls)
+
+# Loop in calls list
+# Worst-case time complexity of this loop is O(n^3)
+for line in calls:
+    if line[0] not in not_telemarket_list:
+        if len(potential_telemarket_list) == 0:
+            potential_telemarket_list.append(line[0])
+        else:
+            if line[0] not in potential_telemarket_list:
+                potential_telemarket_list.append(line[0])
+
+# Sort this list. Worst-case time complexity is O(n log n)
+potential_telemarket_list = sorted(potential_telemarket_list)
+
+# Printing potential telemarket
+# Worst-case Time complexity is O(n)
+txt = "These numbers could be telemarketers: \n"
+for number in sorted(potential_telemarket_list):
+    txt += number + "\n"
+print(txt)
+
+
